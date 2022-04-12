f3a6b7b6c01bd7ccfa604ebcaf0492525e4acc40
FZQ0003 <468403876@qq.com>
2022-04-12 23:37:42 +0800

    Add module: Nudge.

---
+++ modules/nudge.py
@@ -0,0 +1,20 @@
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.mirai import NudgeEvent
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+from utils.config import bot
+
+channel = Channel.current()
+
+channel.name("Nudge")
+channel.description("No description.")
+channel.author("F_Qilin")
+
+
+@channel.use(ListenerSchema(listening_events=[NudgeEvent]))
+async def getup(app: Ariadne, event: NudgeEvent):
+    if event.context_type == "group" and event.target == bot.account:
+        await app.sendNudge(event.supplicant, event.group_id)
+    elif event.context_type == "friend":
+        await app.sendNudge(event.friend_id)
